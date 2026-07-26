f7d865641d87b041196b3b5d40efd4fc0c706d82
Gabriel Cha <95925660+gabrielchasukjin@users.noreply.github.com>
2026-07-26 14:48:45 -0400

    Add: course-schedule - submission-4

---
+++ Data Structures & Algorithms/course-schedule/submission-4.py
@@ -0,0 +1,41 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+        visited = set()
+        stack = [] 
+
+        graph = {}
+        for a, b in prerequisites: 
+            if a in graph.keys():
+                graph[a].append(b)
+            else:
+                graph[a] = [b]
+
+        def dfs(node): 
+            
+            if node in stack:
+                return False 
+            if node in visited:
+                return True
+            
+            visited.add(node)
+            stack.append(node)
+            
+            if node in graph:
+                for neighbor in graph[node]: 
+                    if not dfs(neighbor):
+                        return False 
+            
+            visited.remove(node)
+            stack.remove(node)
+
+            return True
+            
+        
+        ans = []
+        for i in range(numCourses):
+            if i not in visited:
+                ans.append(dfs(i))
+        # print(ans)
+        return all(ans)
+        
